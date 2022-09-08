82da9f0353943838be0f04ceddb04a1083695285
Motanovici <daniel.constantin47@yahoo.com>
2022-09-09 02:04:25 +0300

    First push to Blink Delivery

---
+++ blink-delivery/src/views/Tab1Page.vue
@@ -0,0 +1,65 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core@4.7.4/css/ionic.bundle.css"/>
+  <style>
+    :root {
+      --ion-safe-area-top: 20px;
+      --ion-safe-area-bottom: 22px;
+    }
+  </style>
+<template>
+  <ion-page>
+    <ion-header translucent>
+      <ion-toolbar>
+        <ion-title>Blink Delivery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Comanda</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-searchbar animated 
+                    placeholder="Descopera poftele de azi"
+                    show-cancel-button="focus"></ion-searchbar>
+    <ion-list inset="true">
+      <ion-card button="true" color="primary">
+          <ion-card-header>
+            <ion-card-title>KungFu King</ion-card-title>
+            <ion-card-subtitle>Specific Asiatic</ion-card-subtitle>
+          </ion-card-header>
+    </ion-card>
+    <ion-card button="true" color="primary">
+          <ion-card-header>
+            <ion-card-title>Toan's</ion-card-title>
+            <ion-card-subtitle>Specific Vietnamez</ion-card-subtitle>
+          </ion-card-header>
+    </ion-card>
+    <ion-card button="true" color="primary">
+          <ion-card-header>
+            <ion-card-title>Casa Romaneasca</ion-card-title>
+            <ion-card-subtitle>Specific Romanesc</ion-card-subtitle>
+          </ion-card-header>
+    </ion-card>
+    <ion-card button="true" color="primary">
+          <ion-card-header>
+            <ion-card-title>Tatu Bar&Grill</ion-card-title>
+            <ion-card-subtitle>Specific Romanesc</ion-card-subtitle>
+          </ion-card-header>
+    </ion-card>
+    </ion-list>
+    
+      
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+//import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  defineComponent({
+  name: 'Tab1Page',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+});
+</script>
---
+++ blink-delivery/src/views/TabsPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="fastFoodOutline" />
+          <ion-label>Comanda</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="searchCircleOutline" />
+          <ion-label>Cauta</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="personCircleOutline" />
+          <ion-label>Contul tau</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { ellipse, personCircleOutline, searchCircleOutline, fastFoodOutline } from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'TabsPage',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  setup() {
+    return {
+      ellipse, 
+      personCircleOutline, 
+      searchCircleOutline,
+      fastFoodOutline,
+    }
+  }
+});
+</script>
